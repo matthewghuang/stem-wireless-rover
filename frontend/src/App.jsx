8df6c7e9f028f62b6a418f3fc544aff932fce006
matthewghuang <matthewhhuang@gmail.com>
2021-02-23 22:41:40 +0000

    frontend: add socket.io client

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,15 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
+import { io } from "socket.io-client"
 
 export const App = () => {
-	return <div className="text-lg">Hello, world</div>
+	const [loading, setLoading] = useState(true)
+
+	useEffect(() => {
+		const socket = io("10.32.239.124:3000")
+		socket.on("connect", () => {
+			setLoading(false)
+		})
+	}, [])
+
+	return loading ? <div className="text-lg">Connecting...</div> : <div></div>
 }
