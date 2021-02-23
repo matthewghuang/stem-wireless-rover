8d264653d61f8139f42b165c2771e03f13b80db3
matthewghuang <matthewhhuang@gmail.com>
2021-02-23 22:03:28 +0000

    add socket.io, remove express
    express is not needed, JAMstack can be used :)

--- web-server/index.ts
+++ web-server/index.ts
@@ -1,14 +1,25 @@
-import express from "express"
-import { Request, Response } from "express"
-
-const app = express()
+import { Server, Socket } from "socket.io"
 
 const PORT = 3000
 
-app.get("/", (req: Request, res: Response) => {
-	res.send("Hello, world!")
+const io = new Server(PORT)
+
+io.on("connection", (socket: Socket) => {
+	console.log("a user connected")
+
+	// raspberry pi will send the image to the socket.io server and the frame
+	// will be sent to all other connected clients
+	socket.on("set_frame", (frame: any) => {
+		// send it out to all other clients
+		socket.broadcast.emit("get_frame", frame)
+	})
+
+	// clients will send a direction instruction, the ESP8266 will wait for
+	// this instruction and act accordingly
+	socket.on("set_input", (dir: number, forward: boolean,
+			reverse: boolean) => {
+		socket.broadcast.emit("get_input", dir, forward, reverse)
+	})
 })
 
-app.listen(PORT, () => {
-	console.log(`wireless-rover listening on ${PORT}`)
-})
+console.log(`listening on port ${PORT}`)
