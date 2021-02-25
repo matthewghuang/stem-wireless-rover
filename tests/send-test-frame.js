4cc6d99c9956a613727cf2317da5de4147c4e460
matthewghuang <matthewhhuang@gmail.com>
2021-02-24 18:09:37 -0800

    tests: update send-test-frame

--- tests/send-test-frame.js
+++ tests/send-test-frame.js
@@ -1,10 +1,9 @@
 const io = require("socket.io-client")
 const fs = require("fs")
 
-const socket = io("10.32.239.124:3000")
+const socket = io("ws://localhost:3000")
 
-const img = fs.readFileSync("./red-square.jpg", "binary")
-
-socket.on("connect", () => {
-	socket.emit("set_frame", "hello")
-})
+setInterval(() => {
+	const img = fs.readFileSync("./test.jpg", "base64")
+	socket.emit("set_frame", img)
+}, 1000)
