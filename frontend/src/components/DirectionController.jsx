c841cc7a8fc6ca72d2b3636298c4d5a7fc67da69
matthewghuang <matthewhhuang@gmail.com>
2021-02-25 17:45:12 -0800

    frontend: turn direction buttons red when active

--- frontend/src/components/DirectionController.jsx
+++ frontend/src/components/DirectionController.jsx
@@ -1,9 +1,4 @@
 import React, { useState, useEffect } from "react"
-import styled from "styled-components"
-
-const Box = styled.button.attrs({
-	className: "flex justify-center items-center text-4xl"
-})``
 
 export const DirectionController = () => {
 	const [direction, set_direction] = useState("")
@@ -15,19 +10,19 @@ export const DirectionController = () => {
 	return (
 		<div className="grid grid-cols-3">
 			<div></div>
-			<DirectionButton direction="up" set={set_direction}>
+			<DirectionButton direction="up" cur={direction} set={set_direction}>
 				↑
 			</DirectionButton>
 			<div></div>
-			<DirectionButton direction="left" set={set_direction}>
+			<DirectionButton direction="left" cur={direction} set={set_direction}>
 				←
 			</DirectionButton>
 			<div></div>
-			<DirectionButton direction="right" set={set_direction}>
+			<DirectionButton direction="right" cur={direction} set={set_direction}>
 				→
 			</DirectionButton>
 			<div></div>
-			<DirectionButton direction="down" set={set_direction}>
+			<DirectionButton direction="down" cur={direction} set={set_direction}>
 				↓
 			</DirectionButton>
 			<div></div>
@@ -36,7 +31,7 @@ export const DirectionController = () => {
 }
 
 const DirectionButton = props => {
-	const { direction, set } = props
+	const { direction, cur, set } = props
 
 	const on_down = () => {
 		set(direction)
@@ -47,13 +42,16 @@ const DirectionButton = props => {
 	}
 
 	return (
-		<Box
+		<button
+			className={`flex justify-center items-center text-4xl ${
+				cur == direction && "bg-red-300"
+			}`}
 			onMouseDown={on_down}
 			onMouseUp={on_up}
 			onTouchStart={on_down}
 			onTouchEnd={on_up}
 		>
 			{props.children}
-		</Box>
+		</button>
 	)
 }
