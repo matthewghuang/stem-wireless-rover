1acd23f9267972af4368ad2c7eb16f8b7363fa24
matthewghuang <matthewhhuang@gmail.com>
2021-03-01 21:43:01 +0000

    frontend: wait for a frame before showing controls

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,21 +5,26 @@ import { init } from "./socket"
 
 export const App = () => {
 	const [loading, set_loading] = useState(true)
+	const [ready, set_ready] = useState(false)
 
 	useEffect(() => {
 		const socket = init()
 
 		socket.on("connect", () => {
 			set_loading(false)
+
+			socket.on("get_frame", () => set_ready(true))
 		})
 	}, [])
 
 	return loading ? (
 		<div className="text-lg">Connecting...</div>
-	) : (
+	) : ready ? (
 		<div className="md:w-3/5 mx-auto">
 			<FrameViewer></FrameViewer>
 			<DirectionController></DirectionController>
 		</div>
+	) : (
+		<div>Waiting for camera...</div>
 	)
 }
